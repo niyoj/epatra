83fe5461bf8e3c3c98e1110eecab58526a309d4f
YogeshAryal <yogesh.772417@thc.tu.edu.np>
2022-12-10 20:01:22 +0545

    feat: navbar

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -8,8 +8,8 @@ const Navbar = () => {
   const handleHamburgerMenu =()=>{
     setIsHamburger(!isHamburger);
   }
-  return <div className="h-20 bg-onprimary-variant text-surface">
-    <nav className="p-4 h-20 shadow-xl md:flex md:items-center md:justify-between">
+  return <div className="h-16 bg-onprimary-variant text-surface">
+    <nav className="p-4 h-16 shadow-xl md:flex md:items-center md:justify-between">
       <div className="flex justify-between items-center">
         <span className="text-2xl">
         <img className="h-10 inline" src="" alt=""/>
@@ -19,7 +19,7 @@ const Navbar = () => {
           {isHamburger ?<IoMdClose color="white" onClick={handleHamburgerMenu}/> :<GiHamburgerMenu color="white" onClick={handleHamburgerMenu}/>}
         </span>
       </div>
-      <ul class={`md:flex md:items-center z-[-1] md:z-auto md:static absolute bg-onprimary-variant w-full left-0 md:w-auto md:py-0 my-4 md:pl-0 pl-7 md:opacity-100 opacity-0 top-[-400px] transition-all ease-in duration-300 ${isHamburger && "top-[40px] z-10 opacity-100"}`}>
+      <ul className={`md:flex md:items-center z-[-1] md:z-auto md:static absolute bg-onprimary-variant w-full left-0 md:w-auto md:py-0 my-4 md:pl-0 pl-7 md:opacity-100 opacity-0 top-[-400px] transition-all ease-in duration-300 ${isHamburger && "top-[40px] z-10 opacity-100"}`}>
         <li className="mx-4 my-6 md:my-0">
           <Link href="/" className="text-xl hover:text-secondary duration-500">Home</Link>
         </li>
