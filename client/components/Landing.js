cd5d8ea64880ec513351dafc97a23b3b711e4c7f
Niyoj Oli <oliniyoj@gmail.com>
2022-12-14 00:03:24 +0545

    improves news card

--- client/components/Landing.js
+++ client/components/Landing.js
@@ -4,7 +4,7 @@ import NewsCard from "./NewsCard";
 
 const Landing = () => {
   return (
-    <NewsCard title="How to code news portal in 24 hours?" summary="Learning computer science helps students thrive in a rapidly changing world—more than 65% of young people will work in jobs that dont currently exist. Opportunities in the classroom and beyond."/>
+    <NewsCard title="How to code news portal in 24 hours?" summary="Learning computer science helps students thrive in a rapidly changing world—more than 65% of young people will work in jobs that dont currently exist. Opportunities in the classroom and beyond." date="Dec 13 2022" />
   );
 };
 
