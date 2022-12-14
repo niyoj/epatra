804d33cd0bd6f659830d651890291d1d18366b9f
Niyoj Oli <oliniyoj@gmail.com>
2022-12-14 16:54:16 +0545

    feat: modifies blog post

--- client/components/Landing.js
+++ client/components/Landing.js
@@ -1,56 +1,3 @@
-// import React from "react";
-// import Head from "next/head";
-// import { useRouter } from "next/router";
-// import NewsCards from "./NewsCards";
-
-// const Landing = ({data}) => {
-//   console.log(data);
-//   const router = useRouter();
-//   return (
-//     <>
-//     <div className="bg-surface-variant">
-//       <div className="py-5 flex flex-row-reverse">
-//       <div className="pr-5">
-//       <button className="my-4 bg-primary text-onprimary px-6 py-2 rounded-md capitalize font-bold w-auto hov:-translate-x-1 disabled:cursor-not-allowed disabled:bg-tertiary"
-//         onClick={() => {
-//           router.push("/login");
-//         }}
-//       >
-//         <a className="">Sign In</a>
-//       </button>  
-//       </div>
-//       <div className="pr-10">
-
-//         <button className="my-4 bg-primary text-onprimary px-6 py-2 rounded-md capitalize font-bold w-auto hov:-translate-x-1 disabled:cursor-not-allowed disabled:bg-tertiary"
-//           onClick={() => {
-//             router.push("/register");
-//           }}
-//         >
-//           <a className="">Register</a>
-//         </button>
-//       </div>
-//       </div>
-//     </div>
-
-
-
-//     <div className="flex flex-wrap">
-//       <Head>
-//         <title>ePatra | The Ultimate News Portal</title>
-//       </Head>
-      
-//           {
-//             data.map((news)=>
-//               <NewsCards key={news.slug} news={news}/>
-//             )
-//           }
-
-          
-
-//     </div>
-//     </>
-//   )
-// }
 import NewsCard from "./NewsCard";
 
 const Landing = ({data}) => {
@@ -58,7 +5,7 @@ const Landing = ({data}) => {
     <div className="flex flex-wrap">
     {
       data.map((news)=>
-        <div className="p-6"><NewsCard key={news.slug} news={news}/></div>
+        <NewsCard key={news.slug} news={news}/>
       )
     }
     </div>
